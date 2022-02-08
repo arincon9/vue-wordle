db20739f82873a9179dd26ce6f5e3745da096e28
Alejandro Rincon <alejandro@joinfightcamp.com>
2022-02-07 20:21:56 -0800

    First commit

--- src/Game.vue
+++ src/Game.vue
@@ -113,7 +113,7 @@ function completeRow() {
       setTimeout(() => {
         grid = genResultGrid()
         showMessage(
-          ['Genius', 'Magnificent', 'Impressive', 'Splendid', 'Great', 'Phew'][
+          ['Alex Level', 'Andrea would be proud', 'Chris nods his head', 'Marissa thinks thats hot', 'hehehehe', 'Woah Javi!'][
             currentRowIndex
           ],
           -1
@@ -179,13 +179,7 @@ function genResultGrid() {
     </div>
   </Transition>
   <header>
-    <h1>VVORDLE</h1>
-    <a
-      id="source-link"
-      href="https://github.com/yyx990803/vue-wordle"
-      target="_blank"
-      >Source</a
-    >
+    <h1>RINCON & FRIENDS WORDLE</h1>
   </header>
   <div id="board">
     <div
